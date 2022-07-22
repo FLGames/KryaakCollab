f121eb585f5572c7e76508fd4129e66704a03f0d
FLGames <ALEXLEKLEC@GMAIL.COM>
2022-07-21 21:11:31 -0300

    reverb

---
+++ Scripts/AudioReverbZone.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(BoxCollider))]
+public class AudioReverbZone : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] reverb;
+    void Start()
+    {
+        GetComponent<BoxCollider>().isTrigger = true;
+
+    }
+
+
+    void OnTriggerExit()
+    {
+        
+        foreach (GameObject obj in reverb)
+        obj.gameObject.SetActive(false);
+        
+    }
+    void OnTriggerEnter()
+    {
+        foreach (GameObject obj in reverb)
+            obj.gameObject.SetActive(true);
+    }
+}
